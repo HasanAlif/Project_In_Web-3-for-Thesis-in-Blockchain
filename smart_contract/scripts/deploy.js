33b7faf9c29d04bfa425e2926c00a3ab5197b1fe
Al-Mehedi Hasan Alif <130339768+HasanAlif@users.noreply.github.com>
2024-12-26 03:31:14 +0600

    commit

--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -1,20 +1,47 @@
-async function main() {
-    const [deployer] = await ethers.getSigners();
+// async function main() {
+//     const [deployer] = await ethers.getSigners();
   
-    console.log("Deploying contracts with the account:", deployer.address);
+//     console.log("Deploying contracts with the account:", deployer.address);
   
-    const balance = await deployer.provider.getBalance(deployer.address);
-    console.log("Account balance:", balance.toString());
+//     const balance = await deployer.provider.getBalance(deployer.address);
+//     console.log("Account balance:", balance.toString());
   
-    const Token = await ethers.getContractFactory("Token");
-    const token = await Token.deploy();
+//     const Token = await ethers.getContractFactory("Token");
+//     const token = await Token.deploy();
   
-    console.log("Token address:", token.address);
-  }
+//     console.log("Token address:", token.address);
+//   }
   
-  main()
-    .then(() => process.exit(0))
-    .catch((error) => {
+//   main()
+//     .then(() => process.exit(0))
+//     .catch((error) => {
+//       console.error(error);
+//       process.exit(1);
+//     });
+
+
+
+
+
+
+
+const main = async () => {
+    const transactionsFactory = await hre.ethers.getContractFactory("Transactions");
+    const transactionsContract = await transactionsFactory.deploy();
+  
+    await transactionsContract.deployed();
+  
+    console.log("Transactions address: ", transactionsContract.address);
+  };
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
       console.error(error);
       process.exit(1);
-    });
+    }
+  };
+  
+  runMain();
